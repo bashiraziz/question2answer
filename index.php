c863486ee3bec269a9e7b0da63b6008e18845e97
Bashir Aziz <bashiraziz@users.noreply.github.com>
2019-06-01 19:14:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
 /*
 	Question2Answer by Gideon Greenspan and contributors
 	http://www.question2answer.org/
